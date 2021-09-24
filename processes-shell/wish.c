e8820703014e60b258d34a7d0fa26cb879d3b922
Brandon Yanofsky <byanofsky@me.com>
2021-09-23 21:01:38 -0700

    (shell) Parse inputs

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,11 +4,41 @@
 #include <string.h>
 #include <errno.h>
 
+// Given a line, parses the whitespace separated
+// parts and stores in buffer. The return value
+// specifies how many parts are parsed.
+// Buffer should be freed by caller.
+int parseline(char* line, char*** buffer) {
+  int count = 0;
+  char* part;
+  char** parts = NULL;
+  // TODO: See strsep man for better way to handle.
+  while (1) {
+    // TODO: This mutates line. Should line be copied?
+    part = strsep(&line, " \n\t");
+    // End of line.
+    if (part == NULL) break;
+    // Empty part.
+    if (*part == '\0') continue;
+    // TODO: Is there a better ways besides realloc?
+    parts = realloc(parts, (count+1) * sizeof(char*));
+    if (parts == NULL) {
+      perror("parseline - realloc");
+      exit(1);
+    }
+    parts[count] = part;
+    count++;
+  }
+  *buffer = parts;
+  return count;
+}
+
 int main(int argc, char* argv[]) {
   // TODO: Handle batch
   char* line = NULL;
   size_t len = 0;
   ssize_t nread;
+  char** partsbuffer = NULL;
 
   while (1) {
     printf("wish> ");
@@ -26,12 +56,16 @@ int main(int argc, char* argv[]) {
       exit(1);
     }
 
-    // TODO: Do we need to define line break?
-    if (strcmp(line, "exit\n") == 0) {
+    parseline(line, &partsbuffer);
+
+    // If command 'exit', then exit.
+    if (strcmp(partsbuffer[0], "exit") == 0) {
       break;
     }
   }
 
   free(line);
+  free(partsbuffer);
   exit(0);
 }
+
